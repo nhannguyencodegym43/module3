263de93432540d6b330931d94e7115a29346dac4
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-06-10 20:42:21 +0700

    test email

--- 9.next-js/lesson13/pages/index.js
+++ 9.next-js/lesson13/pages/index.js
@@ -1,11 +1,13 @@
 import Bai1 from "./bai1/bai1";
 import Bai2 from "./bai2/bai2";
+import Bai3 from "./bai3/bai3";
 
 export default function Lesson13 () {
     return (
         <>
             <Bai1 />
             <Bai2 />
+            <Bai3 />
         </>
     )
 }
